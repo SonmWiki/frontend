b01bec740c2282e75c386a1af100ff241444ddd3
Andiv <div2005.kk@gmail.com>
2024-01-21 22:07:37 +0200

    feat: Logo in header

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -2,8 +2,9 @@
 </script>
 
 <template>
-  <header class="flex flex-row gap-2 surface border-bottom-1 surface-border mb-4 p-4 align-items-center">
-    <b>Temporary Header</b>
+  <header class="logo flex flex-row gap-2 border-bottom-1 surface-border p-2 align-items-center">
+    <img class="p-overlay-badge flex align-items-center justify-content-center" src="https://scmc.dev/img/catfish.png" width=48 />
+    <b class="2">Wiki</b>
     <RouterLink to='/' class="link-primary">Home</RouterLink>
     <RouterLink to='/register' class="link-primary">Register</RouterLink>
     <RouterLink to='/login' class="link-primary">Login</RouterLink>
@@ -12,5 +13,15 @@
 </template>
 
 <style scoped>
+header {
+  height: 80px;
+  background: var(--bluegray-900);
+  position: sticky;
+  position: -webkit-sticky;
+  top: 0;
+}
 
+img {
+  object-fit: contain;
+}
 </style>
