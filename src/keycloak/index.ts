4d66ca59f86b71155b1cf024c517e7b5fdba9f29
Andiv <div2005.kk@gmail.com>
2024-01-26 13:52:52 +0200

    fix: fun for env

--- src/keycloak/index.ts
+++ src/keycloak/index.ts
@@ -1,12 +1,12 @@
 import Keycloak from "keycloak-js";
 import type {App} from "vue";
 
-const env = import.meta.env
+const env = () => import.meta.env
 
 const initOptions = {
-    url: env.VITE_KEYCLOAK_URL,
-    realm: env.VITE_KEYCLOAK_REALM,
-    clientId: env.VITE_KEYCLOAK_CLIENT_ID,
+    url: env().VITE_KEYCLOAK_URL,
+    realm: env().VITE_KEYCLOAK_REALM,
+    clientId: env().VITE_KEYCLOAK_CLIENT_ID,
 };
 
 const keycloak = new Keycloak(initOptions);
