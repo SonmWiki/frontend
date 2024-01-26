f4a650715bf9b4cfb3887db23c651b7885fb76c5
Andiv <div2005.kk@gmail.com>
2024-01-26 13:54:12 +0200

    feat: moved to axios

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,18 @@
 <script setup lang="ts">
-import {provide} from "vue";
+import {inject, provide} from "vue";
 import {Api} from "@/api";
 import HeaderComponent from "@/components/HeaderComponent.vue";
+import type Keycloak from "keycloak-js";
 
-const api = new Api();
-api.baseUrl = import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL : `http://${window.location.hostname}:5216`
+const keycloak = inject('keycloak') as Keycloak
+
+const api = new Api()
+api.instance.defaults.baseURL = import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL : `http://${window.location.hostname}:5216`
+
+if (keycloak.token !== undefined)
+  api.instance.defaults.headers.common = {
+    'Authorization': `Bearer ${keycloak.token}`
+  };
 
 provide("api", api)
 </script>
