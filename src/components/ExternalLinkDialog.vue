26dff6e84aec4144893df52a2171223fbe812c34
Andiv <div2005.kk@gmail.com>
2024-02-03 15:45:58 +0200

    feat: Moved External Link Dialog logic

--- src/components/ExternalLinkDialog.vue
+++ src/components/ExternalLinkDialog.vue
@@ -1,17 +1,50 @@
 <script setup lang="ts">
-// TODO: Перекидывать сюда ссылку и оброблять её тута
+import {useConfirm} from "primevue/useconfirm";
+import {type ModelRef, ref, watch} from "vue";
+
+const url: ModelRef<undefined | string> = defineModel("url")
+const shownUrl = ref("")
+
+const confirm = useConfirm();
+
+const showDialog = () => {
+  if (url.value == undefined) return
+
+  shownUrl.value = url.value.substring(0, 36)
+  if (url.value.length > 36) shownUrl.value += ".."
+
+  confirm.require({
+    accept: () => {
+      window.open(url.value, '_blank')
+      reset()
+    },
+    reject: () => {
+      reset()
+    }
+  });
+}
+
+const reset = () => {
+  confirm.close()
+  url.value = undefined
+}
+
+watch(
+    () => url.value,
+    () => {if (url.value != undefined) showDialog()}
+)
 </script>
 
 <template>
   <ConfirmDialog>
-    <template #container="{ message, acceptCallback, rejectCallback }">
+    <template #container="{ acceptCallback, rejectCallback }">
         <div class="flex flex-column p-4">
           <div class="flex flex-column align-items-center w-full gap-3 border-bottom-1 surface-border">
             <i class="pi pi-exclamation-circle text-4xl text-primary-500"></i>
             <div class="flex flex-column gap-2 justify-content-center align-items-center mb-4">
               <div class="text-2xl">External Link</div>
               <div>By continuing, new tab will be opened</div>
-              <div class="link-primary hover:no-underline cursor-auto">{{ message.message }}</div>
+              <div class="link-primary hover:no-underline cursor-auto">{{ shownUrl }}</div>
             </div>
           </div>
           <div class="flex align-items-center gap-2 mt-4 justify-content-center ">
