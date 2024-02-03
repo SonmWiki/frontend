34a778be01fb67fee5202bd14130e52f586adcf3
Andiv <div2005.kk@gmail.com>
2024-02-03 15:45:00 +0200

    feat: Updated API

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,10 +1,11 @@
 import {Api} from "@/api/index";
+import {authService} from "@/main";
 
 const apiInstance = new Api()
 apiInstance.instance.defaults.baseURL = import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL : `http://${window.location.hostname}`
 
 export const api = () => {
-    const token = localStorage.getItem("token")
+    const token = authService.getAccessToken()
 
     if (token != undefined)
         apiInstance.instance.defaults.headers.common = {
