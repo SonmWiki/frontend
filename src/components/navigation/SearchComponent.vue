f616ffcf1ad435ba3d009dedf480638fc394eb43
Andiv <div2005.kk@gmail.com>
2024-02-23 15:43:58 +0200

    feat: New Search Component

--- src/components/navigation/SearchComponent.vue
+++ src/components/navigation/SearchComponent.vue
@@ -1,32 +1,67 @@
 <script setup lang="ts">
 import { api } from '@/api/api'
-import { ref } from 'vue'
-import router from '@/router'
-import type { AutoCompleteCompleteEvent, AutoCompleteItemSelectEvent } from 'primevue/autocomplete'
+import {ref, watch} from 'vue'
 import type { SearchArticlesResponseElement } from '@/api'
 
+const active = ref(false)
 const value = ref("")
 const foundArticles = ref(new Array<SearchArticlesResponseElement>)
 
-async function search(event: AutoCompleteCompleteEvent){
+async function search(query: string){
   try {
-    api().api.searchArticles({searchTerm: event.query}).then((res) => {
+    api().api.searchArticles({searchTerm: query}).then((res) => {
       foundArticles.value = res.data.data
     })
   } catch (err: any) {
     console.log(err.message)
   }
 }
-async function select(event: AutoCompleteItemSelectEvent){
-  await router.push({name: 'articles', params: {id: event.value.id}})
-}
+
+watch(
+    () => value.value,
+    async () => {
+      const curVal = value.value
+      await (new Promise(r => setTimeout(r, 1000)))
+      if (curVal == value.value) await search(value.value)
+    }
+)
 </script>
 
 <template>
-  <InputGroup>
-    <AutoComplete placeholder="Search" v-model="value" optionLabel="articleTitle" :suggestions="foundArticles" @complete="search" @item-select="select" />
-    <InputGroupAddon><i class="pi pi-search"></i></InputGroupAddon>
-  </InputGroup>
+  <Button
+      severity="secondary"
+      aria-haspopup="true"
+      aria-controls="overlay_menu"
+      text
+      icon="pi pi-search"
+      class="flex align-items-center justify-content-center"
+      @click="active = true"
+  />
+
+  <Dialog v-model:visible="active" modal maximizable header="Search" class="w-full md:w-30rem" :breakpoints="{ '1199px': '75vw', '575px': '95vw' }">
+
+    <IconField iconPosition="left">
+      <InputIcon>
+        <i class="pi pi-search" />
+      </InputIcon>
+      <InputText placeholder="Search" v-model="value" class="w-full" />
+    </IconField>
+
+    <div v-if="foundArticles.length > 0" class="flex gap-2 flex-column">
+      <div class="text-primary font-bold pt-3">
+        Results
+      </div>
+      <RouterLink
+          v-for="article in foundArticles"
+          :key="article.id"
+          :to="{name: 'articles', params: {id: article.id}}"
+          class="w-full border-bottom-1 surface-border p-3 border-round no-underline text-color hover:surface-hover"
+          @click="active = false"
+      >
+        {{ article.articleTitle }}
+      </RouterLink>
+    </div>
+  </Dialog>
 </template>
 
 <style scoped>
