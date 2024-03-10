fb126750343f9e3cb023d93a471dabd35dff71bd
VitSun666 <61446535+VitSun666@users.noreply.github.com>
2024-03-10 04:24:08 +0300

    style, chore: restyled and removed unused code from IconSelector

--- src/components/iconSelector/IconSelectorComponent.vue
+++ src/components/iconSelector/IconSelectorComponent.vue
@@ -1,16 +1,12 @@
 <script setup lang="ts">
-import { computed, ref } from 'vue';
-import { PrimeIcons } from 'primevue/api';
-import { AppConstants } from '@/constants/AppConstants';
+import { computed, ref } from "vue"
+import { PrimeIcons } from "primevue/api"
+import { AppConstants } from "@/constants/AppConstants"
 
-const visible = defineModel<boolean>('visible')
-const selectedIcon = defineModel<string | undefined>('selectedIcon')
+const visible = defineModel<boolean>("visible")
+const selectedIcon = defineModel<string | null>("selectedIcon")
 const icons = ref(Object.values(PrimeIcons) as Array<string>)
 const filter = ref()
-
-const emit = defineEmits<{
-  iconSelected: [icon: string | undefined]
-}>()
 
 const filteredIcons = computed(() => {
   if (filter.value) return icons.value.filter((icon) => icon.indexOf(filter.value.toLowerCase()) > -1)
@@ -19,7 +15,14 @@ const filteredIcons = computed(() => {
 </script>
 
 <template>
-  <Dialog @update:hide="selectedIcon = undefined" v-model:visible="visible" modal maximizable header="Select an icon" class="w-full md:w-30rem" :breakpoints="AppConstants.dialogBreakpoints">
+  <Dialog 
+    v-model:visible="visible" 
+    modal 
+    maximizable 
+    header="Select an icon" 
+    class="w-full md:w-30rem"
+    :breakpoints="AppConstants.dialogBreakpoints"
+  >
     <template #header>
       <div class="inline-flex align-items-center justify-content-center gap-2">
         <span class="font-bold white-space-nowrap">Select an icon</span>
@@ -29,8 +32,19 @@ const filteredIcons = computed(() => {
     <div class="card">
       <InputText v-model="filter" class="w-full sticky top-0 z-1 p-3 mb-4" placeholder="Search an icon" />
       <div class="grid justify-content-center text-center">
-        <Button outlined plain text @click="selectedIcon = undefined; emit('iconSelected', selectedIcon)" class="col-2 m-2 w-3rem h-3rem border-5 md:col-2"/>
-        <Button text plain @click="selectedIcon = icon; emit('iconSelected', selectedIcon)" :icon="icon" v-for="icon of filteredIcons" :key="icon" class="col-2 m-2 w-3rem h-3rem md:col-2"/>
+        <Button
+          :severity="selectedIcon === null ? 'primary' : 'secondary'"
+          class="col-2 m-2 w-3rem h-3rem border-5 md:col-2"
+          @click="selectedIcon != null ? selectedIcon = null : {};"
+        />
+        <Button
+          v-for="icon of filteredIcons"
+          :key="icon"
+          :severity="selectedIcon == icon ? 'primary' : 'secondary'"
+          :icon="icon"
+          class="col-2 m-2 w-3rem h-3rem md:col-2"
+          @click="selectedIcon != icon ? selectedIcon = icon:{};"
+        />
       </div>
     </div>
   </Dialog>
