f770bbd9228693da6c69b57f218a75950f658614
VitSun666 <61446535+VitSun666@users.noreply.github.com>
2024-03-11 06:48:12 +0300

    Revert "feat: changed eslint vue3 plugin rule from essential to recommended"
    This reverts commit 0e567ccf184574fb8d83606305ff13d403ab5161.

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,8 +4,8 @@ require('@rushstack/eslint-patch/modern-module-resolution')
 module.exports = {
   root: true,
   'extends': [
+    'plugin:vue/vue3-essential',
     'eslint:recommended',
-    'plugin:vue/vue3-recommended',
     '@vue/eslint-config-typescript',
     '@vue/eslint-config-prettier/skip-formatting'
   ],
