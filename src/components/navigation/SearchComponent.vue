92df24edb729ab703ae39d78239ecd9db719e50c
VitSun666 <61446535+VitSun666@users.noreply.github.com>
2024-05-02 04:09:17 +0300

    fix: missing changes in SearchComponent

--- src/components/navigation/SearchComponent.vue
+++ src/components/navigation/SearchComponent.vue
@@ -1,63 +1,69 @@
 <script setup lang="ts">
-import { api } from '@/api/api'
-import {ref, watch} from 'vue'
-import type { SearchArticlesResponseElement } from '@/api'
-import { AppConstants } from '@/constants/AppConstants'
+import { type Ref, ref, watch } from "vue"
+import type { SearchArticlesResponseElement } from "@/api"
+import { wikiApi } from "@/service/WikiApiService"
 
 const active = ref(false)
 const value = ref("")
-const foundArticles = ref(new Array<SearchArticlesResponseElement>)
+const foundArticles: Ref<Array<SearchArticlesResponseElement>> = ref([])
 
-async function search(query: string){
+const search = async (query: string) => {
   try {
-    api().api.searchArticles({searchTerm: query}).then((res) => {
-      foundArticles.value = res.data.data
-    })
+    foundArticles.value = (await wikiApi.api.searchArticles({ searchTerm: query })).data.data
   } catch (err: any) {
     console.log(err.message)
   }
 }
 
-watch(
-    () => value.value,
-    async () => {
-      const curVal = value.value
-      await (new Promise(r => setTimeout(r, 1000)))
-      if (curVal == value.value) await search(value.value)
-    }
+watch(value, async () => {
+    const curVal = value.value
+    await (new Promise(r => setTimeout(r, 1000)))
+    if (curVal == value.value) await search(value.value)
+  }
 )
+
+watch(active, () => {
+  if (active.value) {
+    search("")
+  }
+})
+
 </script>
 
 <template>
   <Button
-      severity="secondary"
-      aria-haspopup="true"
-      aria-controls="overlay_menu"
-      text
-      icon="pi pi-search"
-      class="flex align-items-center justify-content-center"
-      @click="active = true"
+    severity="secondary"
+    aria-haspopup="true"
+    aria-controls="overlay_menu"
+    text
+    icon="pi pi-search"
+    class="flex align-items-center justify-content-center"
+    @click="active = true"
   />
 
-  <Dialog v-model:visible="active" modal maximizable header="Search" class="w-full md:w-30rem" :breakpoints="AppConstants.dialogBreakpoints">
+  <Dialog
+    v-model:visible="active"
+    modal
+    maximizable
+    header="Search"
+    class="w-full md:w-30rem"
+    :breakpoints="{ '1199px': '75vw', '575px': '95vw' }"
+  >
 
-    <IconField iconPosition="left">
-      <InputIcon>
-        <i class="pi pi-search" />
-      </InputIcon>
-      <InputText placeholder="Search" v-model="value" class="w-full" />
+    <IconField icon-position="left">
+      <InputIcon class="pi pi-search" />
+      <InputText v-model="value" placeholder="Search" class="w-full" />
     </IconField>
-
     <div v-if="foundArticles.length > 0" class="flex gap-2 flex-column">
       <div class="text-primary font-bold pt-3">
         Results
       </div>
       <RouterLink
-          v-for="article in foundArticles"
-          :key="article.id"
-          :to="{name: 'articles', params: {id: article.id}}"
-          class="w-full border-bottom-1 surface-border p-3 border-round no-underline text-color hover:surface-hover"
-          @click="active = false"
+        v-for="article in foundArticles"
+        :key="article.id"
+        :to="{name: 'articles', params: {id: article.id}}"
+        class="w-full border-bottom-1 surface-border p-3 border-round no-underline text-color hover:surface-hover"
+        @click="active = false"
       >
         {{ article.articleTitle }}
       </RouterLink>
