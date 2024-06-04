7b48be3339442da620aa0a8e01e5b89716249318
VitSun666 <61446535+VitSun666@users.noreply.github.com>
2024-06-04 04:46:31 +0300

    feat: access to editor for existing articles

--- src/router/index.ts
+++ src/router/index.ts
@@ -43,16 +43,9 @@ const router = createRouter({
       ]
     },
     {
-      name: "createArticle",
-      path: "/articles/editor",
+      name: "articleEditor",
+      path: "/articles/editor/:articleId?",
       component: ArticleEditorView,
-      meta: { allowedRoles: [UserRole.ADMIN, UserRole.EDITOR, UserRole.USER] }
-    },
-    {
-      name: "editArticle",
-      path: "/articles/editor/:articleId",
-      component: ArticleEditorView,
-      props: true,
       meta: { allowedRoles: [UserRole.ADMIN, UserRole.EDITOR, UserRole.USER] }
     },
     {
