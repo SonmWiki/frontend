fddf3502bd34118560e014d9abd461995c6c54c4
Andiv <div2005.kk@gmail.com>
2024-06-04 14:39:47 +0300

    feat: TypeScript IntelliSense for user-defined env variables

--- env.d.ts
+++ env.d.ts
@@ -1,1 +1,15 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+    readonly VITE_APP_NAME: string,
+    readonly VITE_APP_FULL_NAME: string,
+    readonly VITE_APP_LOGO: string,
+    readonly VITE_API_URL: string,
+    readonly VITE_KEYCLOAK_URL: string,
+    readonly VITE_KEYCLOAK_REALM: string,
+    readonly VITE_KEYCLOAK_CLIENT_ID: string,
+}
+
+interface ImportMeta {
+    readonly env: ImportMetaEnv
+}
